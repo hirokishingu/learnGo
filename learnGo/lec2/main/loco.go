cbd74e8a9a4fc75bb38cf96d1b656b22f72b6495
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-03-07 06:56:55 +0900

    rune

---
+++ learnGo/lec2/main/loco.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+  i := 0
+  for {
+    i++
+    if i%2 == 0{
+      continue
+    }
+    fmt.Println(i)
+    if i >= 10{
+      break
+    }
+    i++
+  }
+}
+
+
+
+
+
+
+
+
+
