7f8183d6f8d3f8d7743c7f2434372d0d2923e4a5
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-03-06 20:50:33 +0900

    understand pointer

---
+++ learnGo/lec1/main/pointer.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  a := 42
+
+  fmt.Println(a)
+  fmt.Println(&a)
+  var b *int = &a
+  fmt.Println(b)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
