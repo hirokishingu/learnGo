acb8b9691b3d3d6d53827361e69da1787679333e
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-03-07 10:54:12 +0900

    slice

---
+++ learnGo/lec4/sli.go
@@ -0,0 +1,23 @@
+package main
+import "fmt"
+
+func main(){
+  mySlice := []int{1, 3, 5, 7, 9, 11}
+  fmt.Printf("%T\n", mySlice)
+  fmt.Println(mySlice)
+  fmt.Println(mySlice[2:5])
+  fmt.Println(mySlice[2])
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
